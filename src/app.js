649f291b9034f11d1daebd50514a98d9765aae70
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-02-17 13:19:53 +0000

    final commit for now

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = generateDomain().join("<br>");
+};
+
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let suffix = [".com", ".tv", ".org"];
+
+  let result = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < suffix.length; l++) {
+          result.push(pronoun[i] + adj[j] + noun[k] + suffix[l]);
+        }
+      }
+    }
+  }
+
+  return result;
 };
